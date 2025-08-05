4e42c01a22d8bc8acc920dac0b1b0a14a5630882
dannyp2293 <it04ubuntu@DESKTOP-KFIODNE.localdomain>
2025-08-05 15:15:58 +0700

    dikantor

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/todo',function(){
+return view('pages.todo.index');
+})->name('todo.index');
+
+Route::get('/edit',function() {
+return view('pages.todo.edit');
+})->name('todo.edit');
